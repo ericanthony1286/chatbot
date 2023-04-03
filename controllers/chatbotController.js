78784d8aeb52a0f01d926e4b515e8b8cc0a54785
Quang <nnquangnd@gmail.com>
2023-04-03 10:47:08 +0700

    second

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -46,6 +46,7 @@ let postWebhook = (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
+        console.log("clgt----------");
         handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
