a23ad8671e7e33577dbc840ad7314f35114d4f23
Quang <nnquangnd@gmail.com>
2023-04-05 14:25:36 +0700

    update 29

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const request = require("request");
 const configViewEngine = require("./config/viewEngine");
 const initWebRoutes = require("./routes/web");
 require("dotenv").config();
@@ -19,6 +20,31 @@ app.use(express.static(path.join(__dirname, "public")));
 // init web routes
 initWebRoutes(app);
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ID = "116542631351987";
+
+const subscribeAppToPage = () => {
+  const options = {
+    method: "POST",
+    uri: `https://graph.facebook.com/${PAGE_ID}/subscribed_apps`,
+    qs: {
+      subscribed_fields: "feed",
+      access_token: PAGE_ACCESS_TOKEN,
+    },
+  };
+
+  request(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      console.log("Webhook subscription successful!");
+    } else {
+      console.error("Failed to subscribe webhook:", error);
+    }
+  });
+};
+
+subscribeAppToPage();
+
+console.log("clgt");
 let port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("server is running ");
