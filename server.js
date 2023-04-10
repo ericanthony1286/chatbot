3d0968ba1073ebb48fe62e9f0aa556ca012f4373
Quang <nnquangnd@gmail.com>
2023-04-10 10:05:16 +0700

    update 71

--- server.js
+++ server.js
@@ -51,30 +51,12 @@ const server = app.listen(port, () => {
   console.log("server is running ");
 });
 
-/* const io = socket(server, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-}); */
 const io = require("./socket-io").init(server);
-/* const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-}); */
-io.on("connection", (socket) => {
+
+/* io.on("connection", (socket) => {
   console.log(`User Connected`);
 
-  /*   socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
- */
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
-});
+}); */
