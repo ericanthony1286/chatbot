45d7e13117ba0deab6d0c14eb03c00359674ae8b
ericanthony1286 <nnquangnd@gmail.com>
2023-04-10 20:49:50 +0700

    update 77

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -91,7 +91,7 @@ let postWebhook = (req, res) => {
               console.log("%%%%%%%%%%%%%%%%%%: ", message);
               handleMessage(sender_psid, message);
             }); */
-            let receiveMessageListeners = io
+       /*      let receiveMessageListeners = io
               .getIO()
               .listeners("receive_message").length;
             if (receiveMessageListeners <= 1) {
@@ -99,7 +99,14 @@ let postWebhook = (req, res) => {
                 console.log("%%%%%%%%%%%%%%%%%%: ", message);
                 handleMessage(sender_psid, message);
               });
+            } */
+            function receiveMessageHandler(message) {
+              console.log("%%%%%%%%%%%%%%%%%%: ", message);
+              handleMessage(sender_psid, message);
+              io.getIO().removeListener("receive_message", receiveMessageHandler);
             }
+          
+            io.getIO().on("receive_message", receiveMessageHandler);
 
             socket.on("disconnect", () => {
               console.log("User Disconnected", socket.id);
